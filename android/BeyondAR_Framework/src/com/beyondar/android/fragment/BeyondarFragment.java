9324eab9f8f937e0f4e0d0e84b31d8e1b5372338
kkkkkv <kevikasa@gmail.com>
2013-11-06 14:14:14 +0900

    added public access methods for cameraView and surfaceView

--- android/BeyondAR_Framework/src/com/beyondar/android/fragment/BeyondarFragment.java
+++ android/BeyondAR_Framework/src/com/beyondar/android/fragment/BeyondarFragment.java
@@ -78,6 +78,25 @@ public class BeyondarFragment extends Fragment implements FpsUpdatable, OnClickL
 	protected CameraView createCameraView() {
 		return new CameraView(getActivity());
 	}
+	
+	/**
+	 * Returns the SurfaceView for this class instance
+	 * 
+	 * @return
+	 */
+	public BeyondarGLSurfaceView getGLSurfaceView(){
+		return mBeyondarGLSurface;
+	}
+	
+	/**
+	 *
+	 * Returns the CameraView for this class instance
+	 * 
+	 * @return
+	 */
+	public CameraView getCameraView(){
+		return mBeyondarCameraView;
+	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {

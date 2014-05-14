bdd53b334ace53b251441c5653f1c9952ce9379f
Joan Puig Sanz <jpuig@comscore.com>
2014-05-14 10:24:54 +0200

    Update documentation for 0.9.2

--- doxygen/search/all_3.js
+++ doxygen/search/all_3.js
@@ -3,7 +3,7 @@ var searchData=
   ['debug_5fopengl',['DEBUG_OPENGL',['../df/d0c/classcom_1_1beyondar_1_1android_1_1util_1_1_logger.html#acc843fa1c3e2f6e909a156dfdc65bd57',1,'com::beyondar::android::util::Logger']]],
   ['debugbitmap',['DebugBitmap',['../df/ddb/classcom_1_1beyondar_1_1android_1_1util_1_1_debug_bitmap.html',1,'com::beyondar::android::util']]],
   ['default_5fcache_5fsize',['DEFAULT_CACHE_SIZE',['../df/d5f/classcom_1_1beyondar_1_1android_1_1util_1_1cache_1_1_bitmap_cache.html#afc37273329896c15998bc795edc2bb01',1,'com::beyondar::android::util::cache::BitmapCache']]],
-  ['default_5fmax_5far_5fview_5fdistance',['DEFAULT_MAX_AR_VIEW_DISTANCE',['../d8/d04/classcom_1_1beyondar_1_1android_1_1world_1_1_world.html#aa2b76613584a29f728c3a72f63341e33',1,'com::beyondar::android::world::World']]],
+  ['default_5fmax_5far_5fview_5fdistance',['DEFAULT_MAX_AR_VIEW_DISTANCE',['../dc/d6d/classcom_1_1beyondar_1_1android_1_1opengl_1_1renderer_1_1_a_r_renderer.html#a64c89036b9f66e8bc7866b7296b73bc3',1,'com::beyondar::android::opengl::renderer::ARRenderer']]],
   ['default_5fplugins_5fcapacity',['DEFAULT_PLUGINS_CAPACITY',['../d5/d7f/interfacecom_1_1beyondar_1_1android_1_1plugin_1_1_plugable_3_01_t_01extends_01_plugin_01_4.html#a67a8e3a005f28501385426c2defa2615',1,'com::beyondar::android::plugin::Plugable&lt; T extends Plugin &gt;']]],
   ['disable',['disable',['../d4/d68/classcom_1_1beyondar_1_1android_1_1util_1_1location_1_1_beyondar_location_manager.html#a8cf905795f425d960130b48bb319f37b',1,'com::beyondar::android::util::location::BeyondarLocationManager']]],
   ['distance',['Distance',['../de/dcc/classcom_1_1beyondar_1_1android_1_1util_1_1math_1_1_distance.html',1,'com::beyondar::android::util::math']]],
